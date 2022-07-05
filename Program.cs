f563815a7b492bb4dbe9b2f37a08c2585074e85c
NicholasDevelop <marcaccini.n@gmail.com>
2022-07-05 14:24:58 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿StreamReader addresses = File.OpenText("C:/Users/black/source/repos/csharp-lista-indirizzi/addresses.csv");
+
+string heading = addresses.ReadLine();
+
+while (!addresses.EndOfStream)
+{
+    string row = addresses.ReadLine();
+    Console.WriteLine(row);
+    string[] data = row.Split(",");
+
+    string name = data[0];
+    string surname = data[1];
+    string street = data[2];
+    string city = data[3];
+    string province = data[4];
+    string zip = data[5];
+}
